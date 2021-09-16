88e643d3005980ec41d66ab5062806d578794316
noah.emmenegger <noah.emmenegger@internezzo.ch>
2021-09-16 16:17:45 +0200

    WIP

--- 04 Zweifaktor-Authentifizierung-mit-TOTP/server.js
+++ 04 Zweifaktor-Authentifizierung-mit-TOTP/server.js
@@ -1,5 +1,8 @@
 var speakeasy = require('speakeasy')
 var express = require('express');
+var qr = require('qr-image');
+
+let users = require('./users.json')
 
 const port = 8080
 
@@ -15,6 +18,25 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/hasUserSecurityToken', (req, res) => {
+    const username = req.query.userName
+    const user = username && users.find(x => x.user == username);
+    res.send(hasUserToken(user))
+})
+
+app.get('/generateSecretForUser', (req, res) => {
+    console.log('ja')
+    const username = req.query.username
+    const user = username && users.find(x => x.user == username);
+    if(!hasUserToken(user)) {
+        // Generate a secret
+        const secret = speakeasy.generateSecret({length: 20})
+        user.secretToken = secret.base32
+
+        res.send('/qrcode?qrurl=' + encodeURIComponent(secret.otpauth_url))
+    }
+})
+
 app.get('/verify', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     
@@ -24,6 +46,20 @@ app.get('/verify', function (req, res) {
         res.end(JSON.stringify({ message: 'Failed to verify' }));
     }
 });
+
+app.get('/qrcode', function(req, res) {
+    var code = qr.image(req.query.qrurl, { type: 'png' });
+    res.type('png');
+    code.pipe(res);
+  });
+
+function hasUserToken(user) {
+    if (user && user.secretToken !== undefined) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function verifyToken(token) {
     console.log(speakeasy.totp({ secret: '4LRW4HZQCC52QP7NIEMCIT4FXYOLWI75', digits: 7, period: 10 }))
