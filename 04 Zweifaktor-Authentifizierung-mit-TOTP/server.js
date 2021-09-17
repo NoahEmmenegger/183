0d5d9a5d0c07a790423ce9024f1cea0bafe20770
noah.emmenegger <noah.emmenegger@internezzo.ch>
2021-09-17 08:42:32 +0200

    format

--- 04 Zweifaktor-Authentifizierung-mit-TOTP/server.js
+++ 04 Zweifaktor-Authentifizierung-mit-TOTP/server.js
@@ -23,9 +23,9 @@ app.get('/hasUserSecurityToken', (req, res) => {
 app.get('/generateSecretForUser', (req, res) => {
     const username = req.query.username
     const user = username && users.find(x => x.user == username);
-    if(!hasUserToken(user)) {
+    if (!hasUserToken(user)) {
         // Generate a secret
-        const secret = speakeasy.generateSecret({length: 20, name: `Key von ${username}`})
+        const secret = speakeasy.generateSecret({ length: 20, name: `Key von ${username}` })
         user.secretToken = secret.base32
 
         res.send('/qrcode?qrurl=' + encodeURIComponent(secret.otpauth_url))
@@ -37,7 +37,7 @@ app.get('/verify', function (req, res) {
 
     const username = req.query.username
     const user = username && users.find(x => x.user == username);
-    
+
     if (verifyToken(user.secretToken, req.query.token)) {
         res.end(JSON.stringify({ message: 'true' }));
     } else {
@@ -45,11 +45,11 @@ app.get('/verify', function (req, res) {
     }
 });
 
-app.get('/qrcode', function(req, res) {
+app.get('/qrcode', function (req, res) {
     var code = qr.image(req.query.qrurl, { type: 'png' });
     res.type('png');
     code.pipe(res);
-  });
+});
 
 app.get('/dashboard', (req, res) => {
     res.send('You successfully loged in. This is your dashboard')
