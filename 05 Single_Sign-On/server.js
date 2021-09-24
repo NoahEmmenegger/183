8ed5eeb10eea39d27fa9bb42a60dc7dac7304be5
Noah Emmenegger <noahemmenegger@gmx.ch>
2021-09-24 12:17:17 +0200

    [WIP] add google sign in

--- 05 Single_Sign-On/server.js
+++ 05 Single_Sign-On/server.js
@@ -1,18 +1,79 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+const cookieParser = require("cookie-parser");
 
-const port = 8080
+const {OAuth2Client} = require('google-auth-library');
+
+const port = 5000
 
 var app = express();
 
+var jsonParser = bodyParser.json()
+
+const CLIENT_ID = '1086787687783-5vfu7ee5o807vp4e844r907b6k67nu53.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
 app.use(express.static(__dirname + '/views'))
+app.engine('html', require('ejs').renderFile);
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.render('index.html')
 })
 
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard.html')
+app.get('/dashboard', checkAuthenticated, (req, res) => {
+    let user = req.user;
+    console.log(user)
+    res.render('dashboard.html', {user})
 })
+
+app.post('/login', jsonParser, (req, res) => {
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: '1086787687783-5vfu7ee5o807vp4e844r907b6k67nu53.apps.googleusercontent.com',
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+    }
+    verify()
+        .then(() => {
+            res.cookie('session-token', token);
+            res.send('success')
+        })
+        .catch(console.error);
+
+})
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
